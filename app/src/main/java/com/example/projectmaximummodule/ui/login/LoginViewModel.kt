a0dc86641306f11378d204979c1e25e85be7d859
dsuniaikn <denis.suniaikin@harman.com>
2022-10-10 00:55:06 +0300

    Fix exception onLogin when something wrong with login or password but Toast not shows

--- app/src/main/java/com/example/projectmaximummodule/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/projectmaximummodule/ui/login/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.projectmaximummodule.ui.login
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.projectmaximummodule.application.AppSharedPreferences

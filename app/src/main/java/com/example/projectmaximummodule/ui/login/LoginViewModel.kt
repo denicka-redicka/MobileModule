fac645094ea402ede3d8ec5bdca5f0f4d8553d05
denicka_redicka <minato01.98@gmail.com>
2022-10-10 01:06:46 +0300

    Provide Theory screen (first version with some issues) Add new Screen for view Theory Item
    Add string resources for Russian Language

--- app/src/main/java/com/example/projectmaximummodule/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/projectmaximummodule/ui/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.projectmaximummodule.ui.login
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.example.projectmaximummodule.application.AppSharedPreferences
